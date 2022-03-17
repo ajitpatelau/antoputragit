70872c40b382d6e46511ea85e94ad05bea7e0a3a
ajitpatelau <ajitpatel@hotmail.com>
2022-03-17 12:33:04 +1100

    added all files and folders

---
+++ main.tf
@@ -0,0 +1,64 @@
+locals {
+  vpc_id           = "vpc-010ed67407053bcce"
+  subnet_id        = "subnet-0aea65b8845362970"
+  ssh_user         = "ubuntu"
+  key_name         = "jenkin"
+  private_key_path = "jenkin.pem"
+}
+
+provider "aws" {
+  region = "ap-southeast-2"
+}
+
+resource "aws_security_group" "nginx" {
+  name   = "nginx_access"
+  vpc_id = local.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "nginx" {
+  ami                         = "ami-0b7dcd6e6fd797935"
+  subnet_id                   = "subnet-0aea65b8845362970"
+  instance_type               = "t2.micro"
+  associate_public_ip_address = true
+  security_groups             = [aws_security_group.nginx.id]
+  key_name                    = local.key_name
+
+  provisioner "remote-exec" {
+    inline = ["echo 'Wait until SSH is ready'"]
+
+    connection {
+      type        = "ssh"
+      user        = local.ssh_user
+      private_key = file(local.private_key_path)
+      host        = aws_instance.nginx.public_ip
+    }
+  }
+  provisioner "local-exec" {
+    command = "ansible-playbook  -i ${aws_instance.nginx.public_ip}, --private-key ${local.private_key_path} nginx.yaml"
+  }
+}
+
+output "nginx_ip" {
+  value = aws_instance.nginx.public_ip
+}
